f957ae0eb74a2603fe1a46aed09cc73387e692e1
Cosimo <cosimo.chellini@gmail.com>
2020-02-16 16:01:12 +0100

    init project

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+type sortable<T> = (first: T, second: T) => number
+type sortableWithOption<T> = (options?: sortOption) => sortable<T>
+
+interface sortOption {
+    desc?: boolean,
+}
+
+
+interface sortByDateOption {
+    desc?: boolean,
+    customParser?: (item: string | number) => Date
+}
+
+const byString: sortableWithOption<string> = (options: sortOption = { desc: false }): sortable<string> => {
+    return (first: string, second: string) => sort(first.localeCompare(second), options)
+}
+
+const byNumber: sortableWithOption<number> = (options: sortOption = { desc: false }): sortable<number> => {
+    return (first: number, second: number) => sort(first - second, options);
+}
+
+const byDate: sortableWithOption<Date | string> = (options: sortByDateOption = { desc: false }): sortable<Date | string> => {
+
+    return (first: Date | string, second: Date | string) => {
+
+        if (typeof first === "string") first = options.customParser(first) ?? new Date(first)
+
+        if (typeof second === "string") second = options.customParser(second) ?? new Date(second)
+
+        return sort(first.getTime() - second.getTime(), options)
+    }
+}
+
+const byValue = <T, TPropType>(discriminator: string | ((item: T) => TPropType), sortFn: sortable<TPropType>): sortable<T> => {
+
+    return (first: T, second: T) => {
+
+        const firstItem: TPropType = getValueByDiscriminator(first, discriminator)
+        const secondItem: TPropType = getValueByDiscriminator(second, discriminator)
+
+        return sortFn(firstItem, secondItem);
+    }
+}
+
+const getValueByDiscriminator = <T, TPropType>(obj: T, discriminator: string | ((item: T) => TPropType)): TPropType => {
+
+    return (typeof discriminator === "string") ? obj[discriminator] : discriminator(obj)
+
+}
+
+const sort = (sortResult: number, options: sortOption) => (options.desc ? 1 : -1) * sortResult
+
+const array = ["134", "pfoiwe", "wopeifpwfoe", "2fiweio"].sort(byString({ desc: false }))
+
+const array2 = [{ id: 1 }, { id: 2 }, { id: 3, ciao: { miao: 23 } }].sort(byValue(c => c?.ciao?.miao, byString({ desc: false })))
