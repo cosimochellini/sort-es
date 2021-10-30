ba6de8c45022b05f640e7f1524715019fec65b39
cosimo chellini <cosimo.chellini@gmail.com>
2021-10-30 19:27:22 +0200

    fix(dependency): updated dependency

--- src/sort.ts
+++ src/sort.ts
@@ -1,4 +1,4 @@
-import { SortOption } from "./interfaces/interfaces";
+import type { SortOption } from "./interfaces/interfaces";
 
 export const getSorter = (options: SortOption): ((result: number) => number) =>
   options.desc ? (result: number) => result * -1 : (result: number) => result;
