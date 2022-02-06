d78880ba5f9ca9ab83a8239a9ecb5571ec08f49e
cosimo chellini <cosimo.chellini@gmail.com>
2022-02-06 17:12:55 +0100

    fix(dep): updated dependency

--- src/sortables/byString.ts
+++ src/sortables/byString.ts
@@ -3,11 +3,11 @@ import { SortByStringOption } from "../interfaces/interfaces";
 import { sortable, sortableWithOption } from "../types/types";
 
 const fixString = (value: string, options: SortByStringOption) => {
-  if (options.lowercase) value = value.toLowerCase();
+    if (options.nullable) value = value || "";
 
-  if (options.nullable) value = value || "";
+    if (options.lowercase) value = value.toLowerCase();
 
-  return value;
+    return value;
 };
 
 /**
@@ -18,18 +18,18 @@ const fixString = (value: string, options: SortByStringOption) => {
  * @version 1.0.0
  */
 const byString: sortableWithOption<string, SortByStringOption> = (
-  options: SortByStringOption = {
-    desc: false,
-    nullable: false,
-    lowercase: false,
-  }
+    options: SortByStringOption = {
+        desc: false,
+        nullable: false,
+        lowercase: false,
+    }
 ): sortable<string> => {
-  const sorter = getSorter(options);
-  return (first: string, second: string): number => {
-    return sorter(
-      fixString(first, options).localeCompare(fixString(second, options))
-    );
-  };
+    const sorter = getSorter(options);
+    return (first: string, second: string): number => {
+        return sorter(
+            fixString(first, options).localeCompare(fixString(second, options))
+        );
+    };
 };
 
 export default byString;
