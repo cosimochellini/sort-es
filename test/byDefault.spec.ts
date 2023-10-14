195dd29fac802ae9a0f2b07e5fe6908d5ea1fff9
John Williams <williamsjohn@microsoft.com>
2023-10-13 23:08:21 -0700

    imrpoved unit tests

--- test/byDefault.spec.ts
+++ test/byDefault.spec.ts
@@ -3,48 +3,23 @@ import { expect } from "chai";
 import byDefault from "../src/sortables/byDefault";
 import fc from "fast-check";
 
-function eq(a: unknown, b: unknown): boolean {
-  // NaN equals itself.
-  if (a !== a && b !== b) {
-    return true;
-  }
-  // Arrays are compared elementwise.
-  if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length !== b.length) {
-      return false;
-    }
-    for (let i = 0; i < a.length; i++) {
-      if (!eq(a[i], b[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return a === b;
-}
-
-const arbPrimitive: fc.Arbitrary<unknown> = fc.oneof(
+const somePrimitive: fc.Arbitrary<unknown> = fc.oneof(
   fc.float(),
-  fc.constant(NaN),
-  fc.constant(Infinity),
-  fc.constant(-Infinity),
-  fc.constant(0),
   fc.string(),
   fc.boolean(),
-  fc.constant(null),
-  fc.constant(undefined),
-  fc.bigIntN(64)
+  fc.bigIntN(64),
+  fc.constantFrom(null, undefined)
 );
 
-const arbSortable: fc.Arbitrary<unknown> = fc.oneof(
-  { weight: 10, arbitrary: arbPrimitive },
-  arbPrimitive.map((p) => ({
+const someSortable: fc.Arbitrary<unknown> = fc.oneof(
+  somePrimitive,
+  somePrimitive.map((p) => ({
     toString: undefined,
     valueOf(): unknown {
       return p;
     },
   })),
-  arbPrimitive.map((p) => ({
+  somePrimitive.map((p) => ({
     valueOf: undefined,
     toString(): unknown {
       return p;
@@ -53,52 +28,51 @@ const arbSortable: fc.Arbitrary<unknown> = fc.oneof(
 );
 
 describe("byDefault", () => {
-  const ascending = byDefault();
-  const descending = byDefault({ desc: true });
+  it(`Treats undefined as equal to itself`, () => {
+    fc.assert(
+      fc.property(fc.boolean(), (desc) => {
+        expect(byDefault({ desc })(undefined, undefined)).equals(0);
+      })
+    );
+  });
 
-  [{ fn: ascending }, { fn: descending }].forEach(({ fn }) => {
-    it(`Treats undefined as equal to itself when ${fn.name}`, () => {
-      expect(ascending(undefined, undefined)).equal(0);
-    });
-
-    it(`Treats undefined as greater than anything else when ${fn.name}`, () => {
-      fc.assert(
-        fc.property(
-          arbSortable.filter((x) => x !== undefined),
-          (x) => {
-            return fn(x, undefined) === -1 && fn(undefined, x) === 1;
-          }
-        )
-      );
-    });
+  it(`Treats undefined as greater than anything else`, () => {
+    fc.assert(
+      fc.property(
+        fc.boolean(),
+        someSortable.filter((x) => x !== undefined),
+        (desc, x) => {
+          const cmp = byDefault({ desc });
+          expect(cmp(x, undefined)).is.below(0);
+          expect(cmp(undefined, x)).is.above(0);
+        }
+      )
+    );
   });
 
   it("Works the same as the default sort order", () => {
     fc.assert(
-      fc.property(arbSortable, arbSortable, (x, y) => {
-        const a1 = [x, y].sort(ascending);
+      fc.property(someSortable, someSortable, (x, y) => {
+        const a1 = [x, y].sort(byDefault());
         const a2 = [x, y].sort();
-        return eq(a1, a2);
-      }),
-      { numRuns: 1000 }
+        expect(a1).deep.equals(a2);
+      })
     );
   });
 
   it("Works in descending mode", () => {
     fc.assert(
-      fc.property(arbSortable, arbSortable, (x, y) => {
+      fc.property(someSortable, someSortable, (x, y) => {
         const a1 = [x, y];
-        a1.sort(descending);
+        a1.sort(byDefault({ desc: true }));
         if (x === undefined) {
-          return eq(a1, [y, undefined]);
+          expect(a1).deep.equals([y, undefined]);
+        } else if (y === undefined) {
+          expect(a1).deep.equals([x, undefined]);
+        } else {
+          expect(a1).deep.equals([x, y].reverse().sort().reverse());
         }
-        if (y === undefined) {
-          return eq(a1, [x, undefined]);
-        }
-        const a2 = [x, y].reverse().sort().reverse();
-        return eq(a2, a1);
-      }),
-      { numRuns: 1000 }
+      })
     );
   });
 });
